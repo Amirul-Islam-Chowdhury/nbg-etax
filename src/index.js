d8a1d8b960ecf1d7c439b98c2ac520ea2e642003
Amirul-Islam-Chowdhury <aislamchy96@gmail.com>
2022-11-16 16:59:15 +0200

    update mobile responsivness

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
